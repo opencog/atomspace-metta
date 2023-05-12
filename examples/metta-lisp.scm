fa34d20eb5a56a1c1ad2d909e835b92805633b62
Linas Vepstas <linasvepstas@gmail.com>
2023-05-11 21:42:24 -0500

    Convert to the new module

--- examples/metta-lisp.scm
+++ examples/metta-lisp.scm
@@ -4,15 +4,16 @@
 ; Experimental demo of a quasi-MeTTa-compatible Lisp-like language.
 ;
 (use-modules (opencog) (opencog exec))
+(use-modules (opencog metta))
 
 ; Define a factorial function
-(LispAst "(= (fact $x) (if (< $x 2) 1 (* $x (fact (- $x 1)))))")
+(MeTTa "(= (fact $x) (if (< $x 2) 1 (* $x (fact (- $x 1)))))")
 
 ; Run it.
-(cog-execute! (LispAst "(fact 5)"))
+(cog-execute! (MeTTa "(fact 5)"))
 
 ; Define a simple named numeric value
-(LispAst "(= foo 6)")
+(MeTTa "(= foo 6)")
 
 ; Run it.
-(cog-execute! (LispAst "(fact foo)"))
+(cog-execute! (MeTTa "(fact foo)"))
--- tests/atoms/metta/metta-basic-test.scm
+++ tests/atoms/metta/metta-basic-test.scm
@@ -3,6 +3,7 @@
 ; Test for the matching `metta-lisp.scm` example demo.
 ;
 (use-modules (opencog) (opencog exec))
+(use-modules (opencog metta))
 (use-modules (opencog test-runner))
 
 (opencog-test-runner)
@@ -10,23 +11,23 @@
 (test-begin tname)
 
 ; Define a factorial function
-(LispAst "(= (fact $x) (if (< $x 2) 1 (* $x (fact (- $x 1)))))")
+(MeTTa "(= (fact $x) (if (< $x 2) 1 (* $x (fact (- $x 1)))))")
 
 ; Run it.
-(define fact5 (cog-execute! (LispAst "(fact 5)")))
+(define fact5 (cog-execute! (MeTTa "(fact 5)")))
 
 ; Expect this
-(define ef5 (LispAst "120"))
+(define ef5 (MeTTa "120"))
 
 (test-assert "factorial-five" (equal? fact5 ef5))
 
 ; Define a simple named numeric value
-(LispAst "(= foo 6)")
+(MeTTa "(= foo 6)")
 
 ; Run it.
-(define fact6 (cog-execute! (LispAst "(fact foo)")))
+(define fact6 (cog-execute! (MeTTa "(fact foo)")))
 ; Expect this
-(define ef6 (LispAst "720"))
+(define ef6 (MeTTa "720"))
 
 (test-assert "factorial-six" (equal? fact6 ef6))
 
